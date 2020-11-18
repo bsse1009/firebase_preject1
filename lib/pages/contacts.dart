d7113c9c0ca84b8084bd162acb9c6224214c1939
bsse1009 <bsse1009@iit.du.ac.bd>
2020-11-19 00:34:40 +0600

    restore data from database!

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -1,4 +1,6 @@
 import 'package:contacts_manager/pages/createNewContact.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class Contacts extends StatefulWidget {
@@ -7,22 +9,136 @@ class Contacts extends StatefulWidget {
 }
 
 class _ContactsState extends State<Contacts> {
+  Query _ref;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _ref = FirebaseDatabase.instance
+        .reference()
+        .child('contacts')
+        .orderByChild('name');
+  }
+
+  Widget _buildContactItem({Map contact}) {
+    Color typeColor = getTypeColor(contact['type']);
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(10),
+      height: 100,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.person,
+                color: Theme.of(context).primaryColor,
+                size: 20,
+              ),
+              SizedBox(
+                width: 6,
+              ),
+              Text(
+                contact['name'],
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.phone_iphone,
+                color: Theme.of(context).accentColor,
+                size: 20,
+              ),
+              SizedBox(
+                width: 6,
+              ),
+              Text(
+                contact['number'],
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(width: 15),
+              Icon(
+                Icons.group_work,
+                color: typeColor,
+                size: 20,
+              ),
+              SizedBox(
+                width: 6,
+              ),
+              Text(
+                contact['type'],
+                style: TextStyle(
+                    fontSize: 16,
+                    color: typeColor,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My Contacts"),
-        ),
-        body: Container(
-          height: double.infinity,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (_) {
-              return AddContacts();
-            }));
+      appBar: AppBar(
+        title: Text('My Contacts'),
+      ),
+      body: Container(
+        height: double.infinity,
+        child: FirebaseAnimatedList(
+          query: _ref,
+          itemBuilder: (BuildContext context, DataSnapshot snapshot,
+              Animation<double> animation, int index) {
+            Map contact = snapshot.value;
+            return _buildContactItem(contact: contact);
           },
-          child: Icon(Icons.add),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) {
+              return AddContacts();
+            }),
+          );
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+    );
+  }
+
+  Color getTypeColor(String type) {
+    Color color = Theme.of(context).accentColor;
+
+    if (type == 'Work') {
+      color = Colors.brown;
+    }
+
+    if (type == 'Family') {
+      color = Colors.green;
+    }
+
+    if (type == 'Friends') {
+      color = Colors.teal;
+    }
+    return color;
   }
 }
